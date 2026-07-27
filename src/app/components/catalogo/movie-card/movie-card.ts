8c87908dbed8b3127aa09b7e3146cf101c77b829
Giorgio <giorgio.smaldino@gmail.com>
2026-07-27 15:57:37 +0200

    fix file mock e aggiunta di nuovi film

--- src/app/components/catalogo/movie-card/movie-card.ts
+++ src/app/components/catalogo/movie-card/movie-card.ts
@@ -15,7 +15,6 @@ export class MovieCard {
   private readonly watchlistService = inject(WatchlistService);
 
   @Input({ required: true }) movie!: Movie;
-  @Input() isInWatchlist = false;
   @Output() addToWatchlist = new EventEmitter<string>();
 
   get isInWatchlist(): boolean {
