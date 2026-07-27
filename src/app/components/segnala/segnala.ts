28ed0f62c1c8cd1bdbe7e6ec49b34395b7a18793
Alex <aletuber123@gmail.com>
2026-07-27 12:49:10 +0200

    SCRUM-20 segnala (#6)
    * feat: form di segnalazione con validation e control flow
    
    * style: aggiornato design del form di segnalazione con css moderno
    
    * feat: SCRUM-20 segnalazione - aggiunto commento

--- src/app/components/segnala/segnala.ts
+++ src/app/components/segnala/segnala.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+// SCRUM-19
+// SCRUM-20 segnalazione
+import { Component, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-segnala',
-  imports: [],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './segnala.html',
   styleUrl: './segnala.css',
 })
-export class Segnala {}
+export class Segnala {
+  private fb = inject(FormBuilder);
+
+  public segnalaForm: FormGroup = this.fb.group({
+    nome: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    movieId: ['', [Validators.required]],
+    tipo: ['', [Validators.required]],
+    descrizione: ['', [Validators.required, Validators.minLength(10)]]
+  });
+
+  public formInviato = false;
+
+  onSubmit() {
+    if (this.segnalaForm.valid) {
+      console.log('Segnalazione inviata:', this.segnalaForm.value);
+      this.formInviato = true;
+      this.segnalaForm.reset();
+      
+      // Nascondi il messaggio di successo dopo qualche secondo
+      setTimeout(() => {
+        this.formInviato = false;
+      }, 5000);
+    }
+  }
+}
