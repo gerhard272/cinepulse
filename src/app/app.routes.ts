6f3671b3feebce6410eff54b90e6bee4ed004227
Nico030908 <gigantenicolo2003@gmail.com>
2026-07-27 13:28:07 +0200

    SCRUM-16 Selezione data/ora/tipo biglietto
    - Select popolato da PrenotazioneService via showtimes.json
    - Radio button per tipo biglietto (intero/ridotto/bambino)
    - Validazione errori con @if su campi touched/invalid
    - Pipe currency EUR/it-IT su prezzi

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'prenotazione',
+    pathMatch: 'full',
+  },
+  {
+    path: 'prenotazione',
+    loadComponent: () =>
+      import('./components/prenotazione/prenotazione').then(
+        (m) => m.PrenotazioneComponent
+      ),
+  },
+  {
+    path: 'prenotazione/:movieId',
+    loadComponent: () =>
+      import('./components/prenotazione/prenotazione').then(
+        (m) => m.PrenotazioneComponent
+      ),
+  },
+];
