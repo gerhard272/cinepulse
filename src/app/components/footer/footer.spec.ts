04f16ac2bf6f63f7e4e865f6285d9b2bbc1339bf
gerhard272 <gerhardpirretti2003@gmail.com>
2026-07-28 16:24:15 +0200

    defined spec.ts of several components for tests purposes

--- src/app/components/footer/footer.spec.ts
+++ src/app/components/footer/footer.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { provideRouter } from '@angular/router';
 import { Footer } from './footer';
 
 describe('Footer', () => {
@@ -9,11 +9,12 @@ describe('Footer', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [Footer],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(Footer);
     component = fixture.componentInstance;
-    await fixture.whenStable();
+    fixture.detectChanges();
   });
 
   it('should create', () => {
